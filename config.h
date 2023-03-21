8734ab4359c60348bbaf97959121bd8a8cb75ebb
CrazyTuner <57575960+CrazyTuner@users.noreply.github.com>
2023-03-21 14:10:03 +0100

    Create config.h

---
+++ config.h
@@ -0,0 +1,8 @@
+const char* ssid = "";
+const char* password = "";  //password to wifi
+const char* mqtt_server = "";             //ip to mqtt server
+const char* mqtt_user = "";                           //mqtt user name
+const char* mqtt_pass = "";                           //mqtt password
+
+#define OTA_HOSTNAME                    "VictronMPPTGarten"
+#define MQTT_ROOT                       "Victron2"
